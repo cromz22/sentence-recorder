bd32c38786cb411212f54a0697fe02550c5f3571
cromz22 <tt.pfr4343@gmail.com>
2025-01-09 13:07:44 +0900

    read json

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pathlib import Path
 
 app = FastAPI()
 
@@ -20,3 +23,19 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "connected to backend"}
+
+
+@app.get("/read-json/{json_stem}")
+def read_json(json_stem: str):
+    data_dir = Path("data")
+    try:
+        json_file = data_dir / f"{json_stem}.json"
+        with open(json_file, "r", encoding="utf-8") as f:
+            content = json.load(f)
+
+        return JSONResponse(content)
+
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail=f"Invalid JSON format: {json_stem}.json.")
